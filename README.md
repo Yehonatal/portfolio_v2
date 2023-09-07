1dc792bdc44ae23247e5c5e659b0d329d54b8770
yonatan afewerk <yonatanafewerk@gmail.com>
2023-09-07 18:27:51 +0300

    react/vite/ts

---
+++ README.md
@@ -0,0 +1,3 @@
+# Portfolio_v2
+
+Building with React, TypeScript... and I don't know yet.
---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+    return (
+        <>
+            <div>
+                <h1>Portfolio_v2</h1>
+            </div>
+        </>
+    );
+}
+
+export default App;
