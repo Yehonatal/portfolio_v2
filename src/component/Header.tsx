c7a44bc2ddd71f86b44b37f1d72b419a9357b92c
yonatan afewerk <yonatanafewerk@gmail.com>
2023-09-08 10:39:14 +0300

    hum menu added

--- src/component/Header.tsx
+++ src/component/Header.tsx
@@ -1,6 +1,7 @@
 import { GithubOutlined } from "@ant-design/icons";
 import "../style/header.css";
 import { FaCloudMoon } from "react-icons/fa";
+import { BiMenuAltLeft } from "react-icons/bi";
 
 function Header() {
     function changeTheme() {
@@ -13,20 +14,36 @@ function Header() {
         }
     }
 
+    function humActions() {
+        if (
+            document
+                .querySelector(".hum_menu_container")
+                ?.classList.contains("hide")
+        ) {
+            document
+                .querySelector(".hum_menu_container")
+                ?.setAttribute("class", "hum_menu_container");
+        } else {
+            document
+                .querySelector(".hum_menu_container")
+                ?.setAttribute("class", "hum_menu_container hide");
+        }
+    }
+
     return (
         <header>
             <div className="header_container">
                 <div className="tag">
-                    <a href="#Home">
+                    <a href="/Home">
                         <h3>Yonatan Afewerk</h3>
                     </a>
                 </div>
                 <nav className="hide_mobile">
                     <li>
-                        <a href="#">Works</a>
+                        <a href="/Works">Works</a>
                     </li>
                     <li>
-                        <a href="#">Posts</a>
+                        <a href="/Posts">Posts</a>
                     </li>
                     <li>
                         <a
@@ -43,6 +60,29 @@ function Header() {
                         <FaCloudMoon />
                     </div>
                 </div>
+                {/* Mobile Menu */}
+                <nav className="hide_desktop">
+                    <div className="hum_menu" onClick={humActions}>
+                        <BiMenuAltLeft />
+                    </div>
+                    <div className="hum_menu_container hide">
+                        <li>
+                            <a href="/Works">Works</a>
+                        </li>
+                        <li>
+                            <a href="/Posts">Posts</a>
+                        </li>
+                        <li>
+                            <a
+                                href="https://github.com/Yehonatal/portfolio_v2"
+                                target="_blank"
+                            >
+                                <GithubOutlined />
+                                Source
+                            </a>
+                        </li>
+                    </div>
+                </nav>
             </div>
         </header>
     );
