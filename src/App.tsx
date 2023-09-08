52c46d6257ae03d9dde053ab4704eea0f8faee7c
yonatan afewerk <yonatanafewerk@gmail.com>
2023-09-08 09:50:09 +0300

    removed lazy loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
 import Header from "./component/Header";
+import Hero from "./component/Hero";
 
-import { Suspense } from "react";
-import LazyHero from "./component/Lazy";
+// import { Suspense } from "react";
+// import LazyHero from "./component/Lazy";
 
 function App() {
     return (
         <>
             <Header />
-            <Suspense fallback={<div>Loading ... </div>}>
-                <LazyHero />
-            </Suspense>
+            <Hero />
         </>
     );
 }
