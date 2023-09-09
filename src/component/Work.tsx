b29446c6458fa9ba77401a93fe0dea5821f70fc3
yonatan afewerk <yonatanafewerk@gmail.com>
2023-09-09 08:41:06 +0300

    more project linked to works page

--- src/component/Work.tsx
+++ src/component/Work.tsx
@@ -1,9 +1,20 @@
 import Project from "./Project";
+import { BiSolidArrowToRight } from "react-icons/bi";
 import "../style/work.css";
 const Work = () => {
     return (
         <>
             <Project />
+            <div className="more_link">
+                <a
+                    href="https://yonatanafewerk.super.site/other-projects"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    More Projects
+                    <BiSolidArrowToRight />
+                </a>
+            </div>
         </>
     );
 };
