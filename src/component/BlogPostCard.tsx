526b3ab8f26898da039f4ba23ed9d40ab8fea029
yonatan afewerk <yonatanafewerk@gmail.com>
2023-09-09 11:28:03 +0300

    added lazy load to posts

--- src/component/BlogPostCard.tsx
+++ src/component/BlogPostCard.tsx
@@ -1,9 +1,10 @@
-import React from "react";
 import "../style/blog.css";
 import { BiSolidArrowToRight } from "react-icons/bi";
+import React, { useState } from "react";
 
 interface BlogPost {
     cover: string;
+    coverSmall: string;
     title: string;
     description: string;
     link: string;
@@ -13,18 +14,36 @@ interface BlogPostCardProps {
     post: BlogPost;
 }
 
-const BlogPostCard: React.FC<BlogPostCardProps> = ({ post }) => (
-    <div className="blog-card">
-        <img src={post.cover} alt={post.title} />
-        <div className="blog-content">
-            <h2>{post.title}</h2>
-            <p>{post.description}</p>
-            <a href={post.link} target="_blank" rel="noopener noreferrer">
-                Read More
-                <BiSolidArrowToRight />
-            </a>
+const BlogPostCard: React.FC<BlogPostCardProps> = ({ post }) => {
+    const [isImgLoaded, setIsImgLoaded] = useState(false);
+
+    const handleImgLoad = () => {
+        setIsImgLoaded(true);
+    };
+
+    return (
+        <div className="blog-card">
+            <div
+                className={`blurred-img ${isImgLoaded ? "loaded" : ""}`}
+                style={{ backgroundImage: `url(${post.coverSmall})` }}
+            >
+                <img
+                    src={post.cover}
+                    alt={post.title}
+                    loading="lazy"
+                    onLoad={handleImgLoad}
+                />
+            </div>
+            <div className="blog-content">
+                <h2>{post.title}</h2>
+                <p>{post.description}</p>
+                <a href={post.link} target="_blank" rel="noopener noreferrer">
+                    Read More
+                    <BiSolidArrowToRight />
+                </a>
+            </div>
         </div>
-    </div>
-);
+    );
+};
 
 export default BlogPostCard;
