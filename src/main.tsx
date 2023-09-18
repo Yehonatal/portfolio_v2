07d0cac43e6f82ca33d30a1afd27d50855b74b35
yonatan afewerk <yonatanafewerk@gmail.com>
2023-09-18 18:39:39 +0300

    vercel analytics added

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/react";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
@@ -6,5 +7,6 @@ import "./style/index.css";
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
         <App />
+        <Analytics />
     </React.StrictMode>
 );
